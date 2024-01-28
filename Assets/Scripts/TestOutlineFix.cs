2e1cd69ebeab13589b5afb90dd25812a054aa8b7
Jesús Martínez <jmh952010@gmail.com>
2024-01-28 16:07:11 +0100

    fix outline

--- Assets/Scripts/TestOutlineFix.cs
+++ Assets/Scripts/TestOutlineFix.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 
 public class TestOutlineFix : MonoBehaviour
 {
-    private Mesh mesh;
-    // Start is called before the first frame update
-    void Start()
-    {
-        MeshFilter viewedModelFilter = (MeshFilter)gameObject.GetComponent("MeshFilter");
-        mesh = viewedModelFilter.sharedMesh;
-        Unwrapping.GenerateSecondaryUVSet(mesh);
-    }
+    //private Mesh mesh;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    MeshFilter viewedModelFilter = (MeshFilter)gameObject.GetComponent("MeshFilter");
+    //    mesh = viewedModelFilter.sharedMesh;
+    //    Unwrapping.GenerateSecondaryUVSet(mesh);
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
 }
